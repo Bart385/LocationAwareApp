d5721612d01546757ab1ce0915a3d9f29cb2037c
Ruben <rwoldhui@avans.nl>
2018-12-12 15:00:38 +0100

    [Added] VolleyService handler
    This service processes a jsonrequest

--- app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/Services/VolleyRequests/IRequest.java
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/Services/VolleyRequests/IRequest.java
@@ -1,4 +1,8 @@
 package com.ruben.woldhuis.androideindopdrachtapp.Services.VolleyRequests;
 
+import com.android.volley.toolbox.JsonRequest;
+
+
 public interface IRequest {
+    JsonRequest doRequest();
 }
