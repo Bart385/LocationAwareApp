6b27f5d5b0c6895ae42d4291fbb64fb460f2bf71
Unknown <bart.vanes1@gmail.com>
2018-12-16 23:49:07 +0100

    [Added] RecyclerView and Contact name for logic WORKING VERSION!!

---
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Activities/ItemFriendActivity.java
@@ -0,0 +1,16 @@
+package com.ruben.woldhuis.androideindopdrachtapp.View.Activities;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.TextView;
+
+public class ItemFriendActivity extends RecyclerView.ViewHolder{
+
+    public TextView textView;
+
+    public ItemFriendActivity(View itemView) {
+            super(itemView);
+            textView = (TextView) itemView;
+    }
+}
+
--- app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Fragments/FriendsFragment.java
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Fragments/FriendsFragment.java
@@ -4,13 +4,24 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.ruben.woldhuis.androideindopdrachtapp.Adapters.FriendsRecyclerAdapter;
+import com.ruben.woldhuis.androideindopdrachtapp.Models.Contact;
 import com.ruben.woldhuis.androideindopdrachtapp.R;
 
+import java.util.ArrayList;
+
 public class FriendsFragment extends Fragment {
+
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
 
     private static FriendsFragment instance;
 
@@ -27,8 +38,22 @@ public class FriendsFragment extends Fragment {
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ArrayList<Contact> friend = new ArrayList<>();
+        friend.add(new Contact("Panda"));
+
         View v = inflater.inflate(R.layout.fragment_friends, container, false);
+
+        mRecyclerView = v.findViewById(R.id.Friend_RecyclerView);
+        mLayoutManager = new LinearLayoutManager(this.getActivity());
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new FriendsRecyclerAdapter(friend);
+        mRecyclerView.setAdapter(mAdapter);
+
         return v;
+
+
+
     }
 
 }
