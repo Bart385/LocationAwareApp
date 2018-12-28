142817f1ab15aa9795fa8b3493e874d84d0c1093
Unknown <bart.vanes1@gmail.com>
2018-12-28 23:37:35 +0100

    [Added] SettingsActivity

--- app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Activities/SettingsActivity.java
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Activities/SettingsActivity.java
@@ -1,8 +1,15 @@
 package com.ruben.woldhuis.androideindopdrachtapp.View.Activities;
 
 import android.app.Activity;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ruben.woldhuis.androideindopdrachtapp.R;
 
 public class SettingsActivity extends Activity {
@@ -11,5 +18,33 @@ public class SettingsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        FirebaseUser firebaseApp = FirebaseAuth.getInstance().getCurrentUser();
+
+        String settingsEmail = firebaseApp.getEmail();
+        String settingsDisplayName = firebaseApp.getDisplayName();
+        String settingsPhoneNumber = firebaseApp.getPhoneNumber();
+        Uri settingsPhotoUri = firebaseApp.getPhotoUrl();
+
+        TextView email = findViewById(R.id.Settings_email);
+        email.setText(settingsEmail);
+
+        TextView displayName = findViewById(R.id.settings_profilename);
+        displayName.setText(settingsDisplayName);
+
+        TextView phoneNumber = findViewById(R.id.settings_phonenumber);
+        phoneNumber.setText(settingsPhoneNumber);
+
+        Button button = findViewById(R.id.settings_confirmbutton);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                
+            }
+        });
+
+
+
     }
 }
