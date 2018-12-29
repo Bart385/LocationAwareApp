b9684907bef46415a0571e208512f568852fd15e
Unknown <bart.vanes1@gmail.com>
2018-12-29 12:03:07 +0100

    [Added] Password forgotten and Dispaly name changing

--- app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Activities/SettingsActivity.java
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Activities/SettingsActivity.java
@@ -1,48 +1,77 @@
 package com.ruben.woldhuis.androideindopdrachtapp.View.Activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.UserProfileChangeRequest;
 import com.ruben.woldhuis.androideindopdrachtapp.R;
 
-public class SettingsActivity extends Activity {
+public class SettingsActivity extends Activity implements View.OnClickListener {
+    FirebaseUser firebaseApp;
+    TextView displayNameTB;
+    TextView email;
+    TextView phoneNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        FirebaseUser firebaseApp = FirebaseAuth.getInstance().getCurrentUser();
+         firebaseApp = FirebaseAuth.getInstance().getCurrentUser();
 
         String settingsEmail = firebaseApp.getEmail();
         String settingsDisplayName = firebaseApp.getDisplayName();
-        String settingsPhoneNumber = firebaseApp.getPhoneNumber();
         Uri settingsPhotoUri = firebaseApp.getPhotoUrl();
 
-        TextView email = findViewById(R.id.Settings_email);
+        email = findViewById(R.id.Settings_email);
         email.setText(settingsEmail);
 
-        TextView displayName = findViewById(R.id.settings_profilename);
-        displayName.setText(settingsDisplayName);
+        displayNameTB = findViewById(R.id.settings_profilename);
+        displayNameTB.setText(settingsDisplayName);
 
-        TextView phoneNumber = findViewById(R.id.settings_phonenumber);
-        phoneNumber.setText(settingsPhoneNumber);
 
-        Button button = findViewById(R.id.settings_confirmbutton);
+
+        findViewById(R.id.settings_confirmbutton).setOnClickListener(this);
+        findViewById(R.id.forgotpassword_button).setOnClickListener(this);
 
         //TODO: Settings update dit werkt nu nog niet want ik kan niet alle dingen aanroepen.
 
-        button.setOnClickListener(view -> {});
 
 
 
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.settings_confirmbutton:
+                if (displayNameTB != null){
+                    UserProfileChangeRequest profileChangeRequest =
+                            new UserProfileChangeRequest.Builder().setDisplayName(String.valueOf(displayNameTB.getText())).build();
+                    firebaseApp.updateProfile(profileChangeRequest).addOnCompleteListener(task -> {
+                        Toast toast = Toast.makeText(getBaseContext(), "Changing DisplayName was succesfull!", Toast.LENGTH_LONG);
+                        toast.show();
+                    });
+                }
+                break;
+            case R.id.forgotpassword_button:
+                Toast toast =  Toast.makeText(getBaseContext(), R.string.newpassword, Toast.LENGTH_LONG);
+                toast.show();
+                FirebaseAuth.getInstance().sendPasswordResetEmail(firebaseApp.getEmail());
+        }
     }
 }
