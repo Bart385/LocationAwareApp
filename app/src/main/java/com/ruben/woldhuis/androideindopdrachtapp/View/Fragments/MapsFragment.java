0237e3991f08bf937b79bbeb3fbc5be797727eab
Unknown <bart.vanes1@gmail.com>
2018-12-31 14:33:58 +0100

    [ADded] Marker doesnt work

--- app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Fragments/MapsFragment.java
+++ app/src/main/java/com/ruben/woldhuis/androideindopdrachtapp/View/Fragments/MapsFragment.java
@@ -9,6 +9,7 @@ import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -17,7 +18,9 @@ import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.ruben.woldhuis.androideindopdrachtapp.MainActivity;
+import com.ruben.woldhuis.androideindopdrachtapp.R;
 
 import java.util.Iterator;
 import java.util.Queue;
@@ -31,6 +34,8 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     private GoogleMap mMap;
     private Queue<Runnable> runnables;
     private android.location.Location location;
+    double latitude;
+    double longitude;
 
     public MapsFragment() {
         // Required empty public constructor
@@ -40,7 +45,10 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getMapAsync(this);
+        SupportMapFragment mapFragment = (SupportMapFragment) getFragmentManager().findFragmentById(R.id.map);
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(this);
+        }
         runnables = new LinkedBlockingQueue<>();
     }
 
@@ -53,20 +61,16 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
             requestPermission(Manifest.permission.ACCESS_FINE_LOCATION, REQUEST_PERMISSIONS_ID);
         else
             mMap.setMyLocationEnabled(true);
-
+        getGps();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(new CameraPosition.Builder()
-                .target(new LatLng(51.59144918270287, 4.775340557098389))
+                .target(new LatLng(location.getLatitude(), location.getLongitude()))
                 .bearing(0)
                 .zoom(15)
                 .build()));
-        LatLngBounds bounds = new LatLngBounds.Builder()
-                .include(new LatLng(51.58651785207713, 4.708317190123125))
-                .include(new LatLng(51.61099091944258, 4.770882942675826))
-                .include(new LatLng(51.56619630165785, 4.864953377246138))
-                .include(new LatLng(51.53379788028693, 4.771831899595782))
-                .build();
+        LatLng currentposition = new LatLng(latitude, longitude);
+        mMap.addMarker(new MarkerOptions().position(currentposition).title("Test"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLng(currentposition));
 
-        mMap.setLatLngBoundsForCameraTarget(bounds);
         mMap.setBuildingsEnabled(false);
         mMap.setIndoorEnabled(false);
         mMap.setMinZoomPreference(13);
@@ -81,11 +85,11 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         mMap.getUiSettings().setCompassEnabled(true);
         mMap.getUiSettings().setMapToolbarEnabled(false);
         mMap.getUiSettings().setMyLocationButtonEnabled(true);
-
         Iterator<Runnable> iterator = runnables.iterator();
         while (iterator.hasNext()) {
             Runnable runnable = iterator.next();
             runnable.run();
+
         }
     }
 
@@ -107,8 +111,8 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
                 if (locationManager != null) {
                     location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
                     if (location != null) {
-                        double latitude = location.getLatitude();
-                        double longitude = location.getLongitude();
+                        latitude = location.getLatitude();
+                        longitude = location.getLongitude();
                         return location;
                     }
                 }
